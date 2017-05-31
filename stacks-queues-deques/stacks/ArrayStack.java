44720862e136e5591d3b9fc1a4b2fde859a677ec
Charles Reid <charlesreid1@gmail.com>
2017-05-31 15:14:24 -0700

    Add proper exception handling to ArrayStack.java

--- stacks-queues-deques/stacks/ArrayStack.java
+++ stacks-queues-deques/stacks/ArrayStack.java
@@ -1,6 +1,16 @@
 import java.util.*;
 import java.io.*;
 
+/** Empty exception */
+class Empty extends Exception{
+	// Note, subclass does not inherit any constructor.
+	// We can define our constructor and call the super constructor,
+	// or we can define no constructor, and always use the default constructor.
+	public Empty(String msg) { 
+		super(msg);
+	}
+}
+
 /** Array-based implementation of stack ADT. 
  *
  * Uses a dynamic array to keep track of the stack.
@@ -11,6 +21,9 @@ import java.io.*;
  *   - pop
  *   - peek
  *   - size
+ *
+ * Exceptions:
+ *	 - Empty exception if empty
  */
 public class ArrayStack { 
 	private int capacity; // Capacity of stack
@@ -26,9 +39,40 @@ public class ArrayStack {
 		// Read ints from file
 		Scanner s = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
 
+		System.out.println("Pushing...");
 		while(s.hasNextInt()) {
 			st.push(s.nextInt());
 		}
+		System.out.println("Done.");
+
+		try {
+			System.out.println("Popping one item...");
+			System.out.println(st.pop());
+
+			System.out.println("Popping ten items...");
+			for(int i=0; i<10; i++) { 
+				System.out.println(st.pop());
+			}
+
+			System.out.println("Emptying stack...");
+			while(st.size()>2) {
+				st.pop();
+			}
+
+			System.out.println("Popping stack with two items...");
+			System.out.println(st.pop());
+			System.out.println(st.pop());
+
+		} catch (Empty e) { 
+			System.out.println(e.getStackTrace());
+		}
+
+		try {
+			System.out.println(st.pop());
+		} catch (Empty e) {
+			System.out.println("What is the sound of an empty stack popping?");
+		}
+
 	}
 
 	/** Create a stack and an Object array for the Stack */
@@ -45,7 +89,7 @@ public class ArrayStack {
 	/** Push an object onto the top of the stack: O(1). */
 	public void push(Object o) { 
 		// Check if full
-		if(capacity==len(data)) {
+		if(capacity==data.length) {
 			resize(capacity*2);
 		}
 		// Increment and set
@@ -53,21 +97,30 @@ public class ArrayStack {
 	}
 
 	/** Pop an object from the top of the stack: O(1). */
-	public Object pop() { 
+	public Object pop() throws Empty { 
+		if(capacity<=1){
+			throw new Empty("Popping an empty stack.");
+		} 
+
 		// Remove item from end of array
 		// (top of stack)
-		Object o = data[capacity];
-		data[capacity] = null;
+		Object o = data[capacity-1];
+		data[capacity-1] = null;
 		capacity--;
+
 		// Check if one quarter empty
-		if(capacity<=(len(data)/4)) {
+		if(capacity<=(data.length/4)) {
 			resize(capacity/2);
 		}
 		return o;
 	}
 
 	/** Return reference to object at top of stack: O(1). */
-	public Object peek() { 
+	public Object peek() throws Empty { 
+		if(capacity<=1) {
+			throw new Empty("Popping an empty stack.");
+		}
+
 		// Return reference to top of stack
 		return data[capacity];
 	}
@@ -80,5 +133,6 @@ public class ArrayStack {
 			newdata[i] = o;
 			i++;
 		}
+		data = newdata;
 	}
 }
