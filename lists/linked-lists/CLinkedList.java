6a9c51971cec16d9f8b1627f3492236ffd1d508d
Charles Reid <charlesreid1@gmail.com>
2017-06-01 00:36:15 -0700

    circular linked list - empty class

---
+++ lists/linked-lists/CLinkedList.java
@@ -0,0 +1,22 @@
+/** Circular linked list implementation. 
+ *
+ * Linked list abstract data type implements:
+ *		- isEmpty
+ *		- size
+ *		- toString 
+ *		- first
+ *		- last
+ *		- addfirst
+ *		- addlast
+ *		- removefirst
+ *
+ * Additional:
+ *		- add
+ *		- remove
+ *		- remove(i)
+ *		- removeFirst
+ *		- removeLast
+ *
+ * */
+public class CLinkedList {
+}
