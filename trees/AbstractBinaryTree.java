8a38a5c1d146b7877faacb0c170574476f2a3d0b
Charles Reid <charlesreid1@gmail.com>
2017-06-10 14:24:17 -0700

    add two abstract classes - abstract tree, abstract binary tree.

---
+++ trees/AbstractBinaryTree.java
@@ -0,0 +1,48 @@
+/** 
+ * Abstract binary tree class. 
+ * 
+ * Implements an abstract binary tree class
+ * that extends the abstract tree class.
+ *
+ * Note that much of the real functionality 
+ * is actually being left to the concrete
+ * implementation of Position and BinaryTree.
+ */
+public class AbstractBinaryTree<E> 
+extends AbstractTree<E> 
+implements BinaryTree<E> {
+
+	public Position<E> sibling(Position<E> p) {
+		Position<E> parent = parent(p);
+		if(parent==null) {
+			// root node
+			return null;
+		} else {
+			if(left(parent)==p) {
+				return right(parent);
+			} else {
+				return left(parent);
+			}
+		}
+	}
+
+	public int numChildren(Position<E> p) {
+		int children = 0;
+		if(left(p)!=null) { children++; }
+		if(right(p)!=null){ children++; }
+		return children;
+	}
+
+	public Iterable<Position<E>> children(Position<E> p) {
+		int cap = 2;
+		List<Position<E>> chillun = new ArrayList<>(cap);
+		if(left(p)!=null) { 
+			chillun.add(left(p));
+		} 
+		if(right(p)!=null) { 
+			chillun.add(right(p));
+		}
+		return chillun;
+	}
+}
+
---
+++ trees/AbstractTree.java
@@ -0,0 +1,88 @@
+/**
+ * Abstract tree class.
+ *
+ * Provides concrete implementation of some 
+ * easy-to-define methods.
+ *
+ * Provides an actual class that can be 
+ * extended, slightly more useful than interfaces,
+ * this method is a good place to define 
+ * concrete implementations of virtual methods 
+ * that are common across all trees.
+ *
+ * Note that this is not an endpoint, but just a stop
+ * along the way toward implementing a full,
+ * concrete tree type.
+ *
+ * This would be simpler, but for the fact that we're
+ * exploring ways of using object oriented programming 
+ * principles and featrues to make things more extensible.
+ */
+public class AbstractTree<E> implements Tree<E> {
+
+	/** Return true if position is internal node. */
+	public boolean isInternal(Position<E> p) { 
+		return numChildren(p)>0;
+	}
+
+	/** Return true if position is leaf node. */
+	public boolean isExternal(Position<E> p) {
+		return numChildren(p)==0;
+	}
+
+	/** Return true if this tree position is root. */
+	public boolean isRoot(Position<E> p) { 
+		return p == root(); 
+	}
+
+	/** Recursive implementation of depth. */
+	public int depth(Position<E> p) { 
+		if(p==root) {
+			// base case
+			return 0; 
+		} else {
+			// recursive case
+			return 1 + depth(parent(p));
+		}
+	}
+
+	/** A bad height method, quadratic complexity class. 
+	 * Depth is called on each leaf.
+	 * Worst case depth is, depth ~ n.
+	 * Worst case height is, n_external ~ n.
+	 * Combined, worst case is ~ n^2.
+	 * */
+	private int heightBad() { 
+		int h = 0;
+		for(Position<E> p : positions()) { 
+			if(isExternal(p)) { 
+				h = Math.max(h, depth(p));
+			}
+		}
+		return h;
+	}
+
+	/** Recursive implementation of height. 
+	 *
+	 * This finds the height in a top-down way,
+	 * extending all the way to the end,
+	 * then accumulating integers as it returns back
+	 * to the original root caller.
+	 * */
+	public int height(Position<E> p) {
+		int h = 0;
+		for(Position<E> c : children(p)) { 
+			// see what new height is for this child 
+			int newh = 1 + height(c);
+			// be greedy
+			h = Math.max(h, newh);
+		}
+		return h;
+	}
+
+	/** Get the height of the entire tree. */
+	public int height() {
+		return height(root());
+	}
+
+}
