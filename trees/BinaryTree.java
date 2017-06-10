1933f8adf259dcc4d1b4abaee09e7ebd22cd6ff7
Charles Reid <charlesreid1@gmail.com>
2017-06-10 14:24:47 -0700

    update tree/binary tree interfaces.

--- trees/BinaryTree.java
+++ trees/BinaryTree.java
@@ -1,12 +1,5 @@
-public interface BinaryTree extends Tree {
-	public Position left(Position p);
-	public Position right(Position p);
-	public Position sibling(Position p);
-	// iterable
-	public ArrayList<Position> children(Position p) { 
-		ArrayList<Position> chilluns = new ArrayList<Position>();
-		chilluns.add(left(p));
-		chilluns.add(right(p));
-		return chilluns;
-	}
+public interface BinaryTree<E> extends Tree<E> {
+	public Position<E> left(   Position<E> p) throws IllegalArgumentException;
+	public Position<E> right(  Position<E> p) throws IllegalArgumentException;
+	public Position<E> sibling(Position<E> p) throws IllegalArgumentException;
 }
--- trees/Tree.java
+++ trees/Tree.java
@@ -1,20 +1,29 @@
 /**
- * Tree abstract data type.
+ * Tree abstract data type interface.
  *
+ * Note that the Position class is not defined here. 
+ * It is defined in the concrete implementation 
+ * of your tree.
  */
-public interface Tree {
-	public Position element();
-	public Position root();
-	public boolean isRoot(Position p);
-	public Position parent(Position p);
-	public int numChildren(Position p);
-	public ArrayList<Position> children(Position p);
-	public boolean isLeaf(Position p);
-	public int size();
-	public int depth(Position p);
-	public boolean isEmpty();
-	// iterable
-	public ArrayList<Position> positions(Position p);
+public interface Tree<E> extends Iterable<E> {
 
-	public int height() { return depth(root()) };
+	public int size();
+	public int depth(Position<E> p) throws IllegalArgumentException;
+	public int height();
+	public boolean isEmpty();
+
+	public Iterator<E> iterator();
+	public Iterable<Position<E>> positions();
+
+	public Iterable<Position<E>> children(Position<E> p);
+
+	public Position<E> element();
+	public Position<E> root();
+
+	public boolean isRoot(Position p) throws IllegalArgumentException;
+	public boolean isInternal(Position p) throws IllegalArgumentException;
+	public boolean isExternal(Position p) throws IllegalArgumentException;
+
+	public Position parent(Position p) throws IllegalArgumentException;
+	public int numChildren(Position p) throws IllegalArgumentException;
 }
