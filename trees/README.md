fb6ec8bb85eac0d3e2eab4ce0ec1a82fd0c82d85
Charles Reid <charlesreid1@gmail.com>
2017-06-11 18:39:18 -0700

    adding tree timing to tree files/code.

--- trees/README.md
+++ trees/README.md
@@ -54,10 +54,30 @@ utility methods to modify the tree structure.
 * delete(p) 
 * attach(p,t1,t2)
 
-
-
-some of  these methods (all of them?) are private. 
+Some of these methods (all of them?) are private. 
 
 ## Mutable Linked Binary Tree
 
 Exposes methods to modify the tree using public methods.
+
+
+
+## Testing
+
+### Tree Testing
+
+Implementation:
+* Once you actually finish struggling through the entire implementation, and it is a LOT of nonsense, most of it type-related,
+* You sit down and you finally are ready to get to work.
+* But then... how do you even traverse this thing?
+* The Node class is protected. The Position interface is the one to use. 
+* What methods are public? What methods are not?
+* I can ask for a BFS iterator with all nodes in BFS order,
+* I can also start with a given tree and a given Position pointer and a given depth and create a recursive method to traverse the tree myself.
+
+
+
+
+
+
+
---
+++ trees/TreeTiming.java
@@ -0,0 +1,334 @@
+import java.util.LinkedList;
+import java.util.Random;
+
+/** 
+ * Tree Timing:
+ * Measure Big-O complexity and run time cost of tree operations. 
+ *
+ * This program is intended to be run on the command line like so:
+ *
+ * java Timing > output.txt
+ *
+ */
+public class TreeTiming {
+
+
+
+	public static void main(String[] args) { 
+		LinkedBinTree<Integer> t = new LinkedBinTree<Integer>();
+		Position<Integer> p = t.addRoot(0);
+
+		Position<Integer> p2 = t.addLeft(p,-7);
+		Position<Integer> p4 = t.addLeft(p2,-30);
+		Position<Integer> p5 = t.addRight(p2,-1);
+
+		Position<Integer> p6 = t.addLeft(p4,-45);
+		Position<Integer> p7 = t.addRight(p4,-25);
+
+		Position<Integer> p3 = t.addRight(p,10);
+
+		Position<Integer> p8 = t.addLeft( p3,8);
+		Position<Integer> p9 = t.addRight(p3,25);
+
+		Position<Integer> p10 = t.addLeft( p8,4);
+		Position<Integer> p11 = t.addRight(p8,9);
+
+		// Iterator syntax is supported
+		for(Integer x : t) {
+			System.out.println("Iterator: element "+x);
+		}
+
+		// Print the tree
+		printPreorder(t, t.root(), 0);
+	}
+
+
+
+	/** Verify rotate linked list method is O(n). */
+	public static void test_rotate() {
+
+		StringBuffer sb = new StringBuffer();
+		sb.append("N, Walltime Rotate (ms), Amortized Rotate (us)\n");
+
+		int ntrials = 1000;
+		Random r;
+
+		// Loop over values of N
+		for(int N = (int)(5E3); N <= (int)(5E5); N+=2500) { 
+
+			Tim tim = new Tim();
+
+			// Trials counter is always k for Kafka
+			for(int k = 0; k<ntrials; k++) { 
+				// Each trial is a different sequence of random numbers,
+				// but the sequence matches between tests of different collection types 
+				r = new Random(k);
+				TLinkedList<Integer> mylist = new TLinkedList<Integer>();
+        		for(int i=0; i<N; i++) { 
+					mylist.addFirst(r.nextInt(100));
+				}
+				mylist.rotate();
+			}
+			double time_total = tim.elapsedms();
+
+			// print amortized cost per 1000 operations, times 1000 ms -> us
+			sb.append( String.format("%d, ",N) );
+			sb.append( String.format("%.3f, ", time_total) );
+			sb.append( String.format("%.3f ", time_total/N*1000) );
+			sb.append("\n");
+		}
+
+		System.out.println(sb.toString());
+	}
+
+
+
+
+
+
+
+
+
+
+	/** Verify reverse linked list method is O(n). */
+	public static void test_reverse() {
+
+		StringBuffer sb = new StringBuffer();
+		sb.append("N, Walltime Reverse (ms), Amortized Reverse (us)\n");
+
+		int ntrials = 1000;
+		Random r;
+
+		// Loop over values of N
+		for(int N = (int)(1E3); N <= (int)(1E5); N+=2500) { 
+
+
+			// builtin linked list:
+			//
+			// b prefix is for built-in datatype
+			Tim tim = new Tim();
+
+			// Trials counter is always k for Kafka
+			for(int k = 0; k<ntrials; k++) { 
+				// Each trial is a different sequence of random numbers,
+				// but the sequence matches between tests of different collection types 
+				r = new Random(k);
+				TLinkedList<Integer> mylist = new TLinkedList<Integer>();
+        		for(int i=0; i<N; i++) { 
+					mylist.addFirst(r.nextInt(100));
+				}
+				mylist.reverse();
+			}
+			double time_total = tim.elapsedms();
+
+			// print amortized cost per 1000 operations, times 1000 ms -> us
+			sb.append( String.format("%d, ",N) );
+			sb.append( String.format("%.3f, ", time_total) );
+			sb.append( String.format("%.3f ", time_total/N*1000) );
+			sb.append("\n");
+		}
+
+		System.out.println(sb.toString());
+	}
+
+
+
+
+
+	/** Compare the add and remove method - adding and removing from list. 
+	 *
+	 *  NOTE: If this is not constant, then add is not O(1) amortized, 
+	 *  and there is something wrong with your implementation.
+	 *  */
+	public static void linked_list_add_remove_test() {
+		StringBuffer sb = new StringBuffer();
+
+		//
+		// Add Remove Amortized Walltime Per 1k Builtin (us), Add Remove Amortized Walltime Per 1k SLL (us), Add Remove Amortized Walltime Per 1k DLL (us)
+		//
+		sb.append("N, Walltime Builtin (us), Walltime SLL (us), Walltime DLL (us)\n");
+		
+		int ntrials = 1000;
+		Random r;
+
+		// Loop over values of N
+		for(int N = (int)(1E3); N <= (int)(1E6); N*=4) { 
+
+			sb.append( String.format("%d, ",N) );
+
+
+			// builtin linked list:
+			//
+			// b prefix is for built-in datatype
+			Tim btim = new Tim();
+
+			// Trials counter is always k for Kafka
+			for(int k = 0; k<ntrials; k++) { 
+				// Each trial is a different sequence of random numbers,
+				// but the sequence matches between tests of different collection types 
+				r = new Random(k);
+				LinkedList<Integer> blist = new LinkedList<Integer>();
+        		for(int i=0; i<N; i++) { 
+					// 75% of the time, we add something. 25% of the time, we remove something.
+					boolean addSomething = r.nextBoolean();
+					if(addSomething) { 
+						blist.add(0,r.nextInt(100));
+					} else {
+						if(!blist.isEmpty()) {
+							blist.remove(0);
+						}
+					}
+				}
+			}
+			double btime_total = btim.elapsedms();
+
+
+
+			// my little singly linked list:
+			//
+			// m prefix is for mine
+			Tim mtim = new Tim();
+
+			for(int k=0; k<ntrials; k++) { 
+				// reproducible random numbers across tests
+				r = new Random(k);
+				TLinkedList<Integer> mylist = new TLinkedList<Integer>();
+        		for(int i=0; i<N; i++) { 
+					// 75% of the time, we add something. 25% of the time, we remove something.
+					boolean addSomething = r.nextBoolean();
+					if(addSomething) { 
+						mylist.addFirst(r.nextInt(100));
+					} else {
+						if(!mylist.isEmpty()) {
+							mylist.removeFirst();
+						}
+					}
+				}
+			}
+			double mtime_total = mtim.elapsedms();
+
+
+
+
+			// doubly linked list:
+			//
+			// d prefix is for doubly
+			Tim dtim = new Tim();
+
+			for(int k=0; k<ntrials; k++) { 
+				r = new Random(k);
+				TLinkedList<Integer> dlist = new TLinkedList<Integer>();
+        		for(int i=0; i<N; i++) { 
+					// 75% of the time, we add something. 25% of the time, we remove something.
+					boolean addSomething = r.nextBoolean();
+					if(addSomething) { 
+						dlist.addFirst(r.nextInt(100));
+					} else {
+						if(!dlist.isEmpty()) {
+							dlist.removeFirst();
+						}
+					}
+				}
+			}
+			double dtime_total = dtim.elapsedms();
+
+			// print amortized cost per 1000 operations, times 1000 ms -> us
+			sb.append( String.format("%.3f, ", btime_total/N*1000) );
+			sb.append( String.format("%.3f, ", mtime_total/N*1000) );
+			sb.append( String.format("%.3f ",  dtime_total/N*1000) );
+
+
+			sb.append("\n");
+
+		}
+
+
+		System.out.println(sb.toString());
+
+	}
+
+
+	/** Compare the add method - appending to the rear of a list. 
+	 *  
+	 *  Compares builtin LinkedList type with self-authored TLinkedList class.
+	 *
+	 *  NOTE: If this is not constant, then add is not O(1) amortized, 
+	 *  and there is something wrong with your implementation.
+	 *  */
+	public static void linked_list_add_test() {
+
+		StringBuffer sb = new StringBuffer();
+
+		// Add Amortized Walltime Per 1k Builtin (us), Add Amortized Walltime Per 1k SLL (us)
+		sb.append("N, Add Builtin (us), Add SLL (us), Add DLL (us)\n");
+		
+		int ntrials = 1000;
+
+		// Loop over some values of N, 
+		// print the total cost,
+		// print the cost per add operation 
+		for(int N = 1024; N < 5e5; N *= 2) { 
+
+
+			// builtin linked list:
+			//
+			// b prefix is for built-in datatype
+			Tim btim = new Tim();
+
+			// Trials counter is always
+			// K for Kafka
+			for(int k = 0; k<ntrials; k++) { 
+				LinkedList<Integer> blist = new LinkedList<Integer>();
+        		for(int i=0; i<N; i++) { 
+					blist.add(i*i);
+				}
+			}
+			double btime_total = btim.elapsedms();
+
+			// singly linked list:
+			//
+			// m prefix is for mine
+			Tim mtim = new Tim();
+
+			// Trials counter is k
+			for(int k=0; k<ntrials; k++) { 
+				TLinkedList<Integer> mylist = new TLinkedList<Integer>();
+        		for(int i=0; i<N; i++) { 
+					mylist.addLast(i*i);
+				}
+			}
+			double mtime_total = mtim.elapsedms();
+
+
+			// doubly linked list:
+			Tim dtim = new Tim();
+
+			// Trials counter is k
+			for(int k=0; k<ntrials; k++) { 
+				TLinkedList<Integer> dlist = new TLinkedList<Integer>();
+        		for(int i=0; i<N; i++) { 
+					dlist.addLast(i*i);
+				}
+			}
+			double dtime_total = dtim.elapsedms();
+
+
+			// Print out the data in CSV format:
+
+			// N
+			sb.append( String.format("%d, ", N) );
+
+			// print cost per 1000 add operations us, times 100 ms -> us
+			sb.append( String.format("%.3f, ",  btime_total/N*1000) );
+			sb.append( String.format("%.3f, ",  mtime_total/N*1000) );
+			sb.append( String.format("%.3f ",   dtime_total/N*1000) );
+			sb.append("\n");
+
+		}
+
+		System.out.println(sb.toString());
+	}
+
+
+
+}
