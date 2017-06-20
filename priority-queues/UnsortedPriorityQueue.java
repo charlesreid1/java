7d1fcff544088fd07fdb9ec8d51cc443b7560cf2
Charles Reid <charlesreid1@gmail.com>
2017-06-19 19:47:55 -0700

    Unsorted priority queue class now works.

--- priority-queues/UnsortedPriorityQueue.java
+++ priority-queues/UnsortedPriorityQueue.java
@@ -1,4 +1,10 @@
-import java.util.LinkedListed;
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Random;
+
+class Empty extends IllegalStateException {}
 
 public class UnsortedPriorityQueue<T> extends PriorityQueueBase<T> { 
 
@@ -11,6 +17,15 @@ public class UnsortedPriorityQueue<T> extends PriorityQueueBase<T> {
 		data = new LinkedList<Item<T>>();
 	}
 
+	/** Return string representation of the items. */
+	public String toString() { return data.toString(); }
+
+	/** Get number of elements in the queue. */
+	public int size() { return data.size(); } /* alt: keep track of it ourselves. */
+	public boolean isEmpty() { return data.isEmpty(); }
+
+
+	/** Add a new item, with key k and value v, to this sorted priority queue. */
 	public void add(int k, T v) {
 		// Make new item
 		Item<T> newest = new Item<T>(k,v);
@@ -19,7 +34,96 @@ public class UnsortedPriorityQueue<T> extends PriorityQueueBase<T> {
 		data.add(newest);
 	}
 
+
+	/** Remove the minimum item from the priority queue. */
+	public T removeMin() throws Empty { 
+		if(isEmpty()) { 
+			throw new Empty();
+		}
+		Iterator<Item<T>> iter = findMin();
+		Item<T> item = iter.next();
+		iter.remove();
+		return item.getValue(); 
+	}
+
+
+	/** Peek the minimum item in the priority queue, but do not remove it. */
+	public T peekMin() throws Empty { 
+		if(isEmpty()) { 
+			throw new Empty();
+		}
+		Iterator<Item<T>> iter = findMin();
+		Item<T> item = iter.next();
+		return item.getValue(); 
+	}
+
+
+
+	/** Find minimum item in priority queue. */
+	private Iterator<Item<T>> findMin() throws Empty { 
+		if(isEmpty()) { 
+			throw new Empty();
+		}
+
+		// We are executing a while loop 
+		// with a cumulative variable 
+		// to find the location of the minimum.
+		// Assertion: at least 1 item in the list.
+		Item<T> minValue = data.get(0);
+		int c = 0;
+		int mindex = c;
+		for(Item<T> canaryValue : data) { 
+			if(canaryValue.compareTo(minValue)<0) { 
+				mindex = c;
+			}
+			c++;
+		}
+		return data.listIterator(c-1);
+	}
+
+
+	/** main method */
 	public static void main(String[] args) { 
+		runTest();
+	}
+
+
+	/** Run through tests with an unsorted priority queue. */
+	public static void runTest() { 
+		System.out.println("****************************");
+		System.out.println("**** unsorted test *********");
+
+		Random r = new Random(10);
+
+		int N = 1000000;
+
+		UnsortedPriorityQueue<Integer> q = new UnsortedPriorityQueue<Integer>();
+
+		System.out.println("Created new unsorted priority queue. Testing addition and removal...");
+		System.out.println("Size is now "+q.size());
+
+		for(int i=0; i<N; i++) { 
+
+			if(i%50000==0) { 
+				System.out.println("Processing item "+i+" of "+N);
+			}
+
+			// Create random priority queue items
+			Integer k = new Integer( r.nextInt() );
+			Integer v = new Integer( r.nextInt() );
+			q.add(k,v);
+
+			// Randomly remove priority queue items
+			while(r.nextBoolean()==false) {
+				if(q.size()>0){
+					q.removeMin();
+				}
+			}
+		}
+
+		System.out.println("Done.");
+		System.out.println("Size is now "+q.size());
+
 	}
 }
 
