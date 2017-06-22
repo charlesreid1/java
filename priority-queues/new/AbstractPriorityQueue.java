cc93788e6f97c091921663d39a7eb9a4dcb54467
Charles Reid <charlesreid1@gmail.com>
2017-06-22 13:55:21 -0700

    Abstract class now references Item class w/o defining.

--- priority-queues/new/AbstractPriorityQueue.java
+++ priority-queues/new/AbstractPriorityQueue.java
@@ -8,9 +8,30 @@
  */
 public abstract class AbstractPriorityQueue<K,V> 
 			implements PriorityQueue<K,V> {
-	
 
+	protected static class PQEntry<K,V> implements Entry<K,V> {
+		private K k;
+		private V v;
+		public PQEntry(K key, V value) { 
+			this.k = key; this.v = value;
+		}
 
+		/* Methods associated with the Entry interface: */
+
+		/** Return the key associated with this element. */
+		public K getKey() { return k; }
+		/** Return the value associated with this element. */
+		public V getValue() { return v; }
+
+		/* Protected utility methods to modify the values (get/set): */ 
+
+		/** Set the key associated with this element. */
+		protected void setKey(K k) { this.k = k; }
+
+		/** Set the value associated with this element. */
+		protected void setValue(V v) { this.v = v; }
+
+	}
 
 
 
