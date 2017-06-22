6064a2166dee5de26131ecbed2672b02d80fa54a
Charles Reid <charlesreid1@gmail.com>
2017-06-22 13:55:56 -0700

    update priority queue interface documentation. no abstract keyword.

--- priority-queues/new/PriorityQueue.java
+++ priority-queues/new/PriorityQueue.java
@@ -1,28 +1,32 @@
-/** Prioirity Queue interface.
+/** Priority Queue interface.
  *
- * This defines the two simplest methods a priority queue can get aaaway with.
+ * This defines the two simplest methods a priority queue can get away with.
+ *
  * This is essentially a barebones list of methods. 
+ *
+ * Note that Item<K,V> is not defined here, it is defined in your concrete implementation.
  */
-public interface PriorityQueue<K,V> { 
+public interface PriorityQueue<K,V> extends Iterable<K> { 
 
 	/** Returns true if the priority queue was changed. */
-	public abstract boolean insert(k,v);
+	public boolean insert(k,v);
 
 	/** Remove and return the minimum element in this queue. */
-	public abstract V removeMin() throws Empty;
+	public V removeMin() throws Empty;
 
 	/** Return, but do not remove, the minimum element in this queue. */
-	public abstract V min() throws Empty;
+	public V peekMin() throws Empty;
+
+	/** Returns a key-based iterator. */
+	public Iterator<K> iterator();
+
+	/** Returns an iterable container with all of the Items in this queue. */
+	public Iterable<Item<K,V>> items();
 
 	/** Returns the number of elements in this queue. */
-	public abstract int size();
+	public int size();
 
 	/** Returns true if there are no elements in this queue. */
-	public boolean isEmpty();
-}
-
-interface Entry<K,V> {
-	K getKey(); 
-	V getValue();
+	public isEmpty();
 }
 
