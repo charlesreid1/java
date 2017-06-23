1a565e59700c85ae80a385babd2cc3836deddcda
Charles Reid <charlesreid1@gmail.com>
2017-06-23 02:57:46 -0700

    Using the PriorityQueue concrete implementation to implement a Stack ADT

---
+++ priority-queues/UseAsStack.java
@@ -0,0 +1,45 @@
+public class UseAsStack { 
+	public static void main(String[] args) { 
+		StackPQ<String> q = new StackPQ<String>();
+		q.push("hello");
+		q.push("world");
+		q.push("this");
+		q.push("is");
+		q.push("the");
+		q.push("earth");
+		while(!q.isEmpty()) {
+			System.out.println(q.pop());
+		}
+	}
+}
+
+// NOTE: extends should go before implements
+class StackPQ<V> extends SortedPQ<Integer,V> implements PriorityQueue<Integer,V> { 
+
+	// Stack can implement a priority queue
+	// by adding items and assigning keys 
+	// based on order of insertion.
+	// Keys should be a negative counter,
+	// to keep them in reverse order added.
+	int counter;
+
+	/** Start with an empty stack. */
+	public StackPQ(){ counter = 0; }
+
+	// Stack ADT methods:
+
+	public void push(V v) {
+		counter++; // start at 1
+		this.insert(new Integer(-1*counter), v);
+	}
+
+	public V pop() {
+		counter--;
+		return removeMin();
+	}
+
+	// Can actually just use this.size()-1 instead of counter.
+	// How do we communicate that insert, removeMin, peekMin, etc 
+	// should probably not be used with this ADT? 
+	// Don't sweat it. Just an exercise.
+}
