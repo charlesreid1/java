21ef39a0e1e75baa9d3ac97522506184607c8802
Charles Reid <charlesreid1@gmail.com>
2017-06-24 00:26:05 -0700

    add KLE ("keys less than or equal to") program draft.

---
+++ priority-queues/KLE.java
@@ -0,0 +1,16 @@
+/**
+ * Key Less than or Equal to.
+ *
+ * Given a heap T and a key k,
+ * give an algorithm to compute 
+ * all the entries in T having a key
+ * less than or equal to k.
+ * Not necessarily in order.
+ */
+public class KLE {
+	public static void main(String[] args) { 
+
+		// do stuff.
+
+	}
+}
