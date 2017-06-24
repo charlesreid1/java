27155cf1d6066455ec8061e7683f7fec39fe1dab
Charles Reid <charlesreid1@gmail.com>
2017-06-24 00:00:29 -0700

    Adding the basic Heap Priority Queue class, no main method so no test but all the basic methods in place and compiling.

---
+++ priority-queues/HeapPQ.java
@@ -0,0 +1,149 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Comparator;
+import java.util.Iterator;
+
+/** Implement a heap-based priority queue.
+ *
+ * This utilizes an array to implement a complete binary tree
+ * to hold sorted elements organized by priority. 
+ *
+ * Useful functionality:
+ * Addition
+ * Post-Addition Up-Heap Bubbling
+ * Removal
+ * Post-Removal Down-Heap Bubbling
+ * Height
+ */
+
+// cmr: <K,V> syntax
+// cmr: extends the abstract type, which implements the interface
+public class HeapPQ<K,V> extends AbstractPriorityQueue<K,V> { 
+	// cmr: Item<K,V> is from AbstractPriorityQueue class file, imported with extends above
+	// protected b/c we are gonna extend this
+	protected ArrayList<Item<K,V>> heap;
+
+	// cmr: constructor calls super first. then makes list.
+	public HeapPQ() { 
+		super();
+		heap = new ArrayList<Item<K,V>>();
+	}
+
+	// cmr: remember this was how we designed our abstract priority queue,
+	// so that all PQ classes could use comparators and compare things however they want.
+	public HeapPQ(Comparator<K> keyComparator) { 
+		super(keyComparator);
+	}
+
+	// cmr: index math to turn a node into its parent/left/right nodes
+	protected int parent(int j) { return (j-1)/2; }
+	protected int left(int j) { return 2*j + 1; } 
+	protected int right(int j) { return 2*j + 2; }
+
+	// cmr: boolean checks if left/right exist in the heap 
+	protected boolean hasLeft(int j) { return left(j) < heap.size(); }
+	protected boolean hasRight(int j) { return right(j) < heap.size(); }
+
+
+
+	// Utility:
+
+	protected void swap(int i, int j) {
+		Item<K,V> temp = heap.get(i);
+		heap.set(i, heap.get(j));
+		heap.set(j, temp);
+	}
+
+	protected void upheap(int j) { 
+		// j==0 is root
+		while(j>0) { 
+			int p = parent(j);
+			if(compare(heap.get(j), heap.get(p))>=0) {
+				break; // heap order property satisfied
+			}
+			swap(j,p);
+			j = p; // continue from parent
+		}
+	}
+
+
+	protected void downheap(int j) { 
+		while(hasLeft((j))) {
+			int leftIndex = left(j);
+			// assume left child is smaller
+			int smallChildIndex = leftIndex;
+			// if right child exists, check if right child is smaller
+			if(hasRight(j)) { 
+				int rightIndex = right(j);
+				if(compare(heap.get(leftIndex), heap.get(rightIndex))>0) {
+					smallChildIndex = rightIndex;
+				}
+			}
+			if(compare(heap.get(smallChildIndex), heap.get(j)) >= 0) { 
+				break; // heap order property satisfied
+			}
+			swap(j, smallChildIndex);
+			j = smallChildIndex; // continue from child
+		}
+	}
+
+
+
+	// Really, the main content of this class
+	// (other than the upheap/downheap/swap methods):
+
+	public void insert(K key, V value) { 
+		checkKey(key);
+		Item<K,V> newitem = new Item<K,V>(key, value);
+		heap.add(newitem);
+		upheap( heap.size() - 1 ); // upheap the newest entry (last item in array)
+	}
+
+	public V removeMin() { 
+		if(heap.isEmpty()) { 
+			throw new Empty();
+		}
+		// before you lose the reference to the minimum item,
+		// get a reference to it.
+		Item<K,V> expunge = heap.get(0);
+		// swap the minimum item with the item that will replace it 
+		swap(0, heap.size()-1); // swap the base of the heap with the last item in the tree
+		heap.remove(heap.size()-1); // remove the last item, which no one cares about anymore
+		downheap(0); // restore heap order priority (HOP)
+		return expunge.getValue(); // return the value of the item we grabbed before removing.
+	}
+
+
+
+	// Required by priority queue interface:
+
+	public int size() { 
+		return heap.size();
+	}
+
+	public V peekMin() {
+		if(heap.isEmpty()) { 
+			throw new Empty();
+		}
+		return heap.get(0).getValue();
+	}
+
+
+
+	protected class ItemIterator implements Iterator<K> {
+		Iterator<Item<K,V>> it;
+		public ItemIterator() { 
+			it = heap.iterator();
+		}
+		public boolean hasNext() { return it.hasNext(); }
+		public K next() { return it.next().getKey(); }
+		public void remove() { it.remove(); }
+	}
+
+	public Iterator<K> iterator() { 
+		return new ItemIterator();
+	}
+
+
+}
