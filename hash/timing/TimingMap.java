264e45079dc22d6cb7ed6594a922e2bb307ccf33
Charles Reid <charlesreid1@gmail.com>
2017-06-27 00:29:28 -0700

    Updating output from TimingMap class, and adding output file.

--- hash/timing/TimingMap.java
+++ hash/timing/TimingMap.java
@@ -9,39 +9,31 @@ public class TimingMap {
 
 	// Static constants:
 
-	// We will perform and time Nops add operations.
+	// We will perform and time Nops insert operations.
 	static int Nops = 100;
 
 	// We will do that Ntrials times.
-	static int Ntrials = 1000;
+	static int Ntrials = 5000;
 
-	// We will add to a container of size N.
-	static int[] Ns = {1000,2000,3000,4000,5000,6000,7000,8000,9000,10000};
+	// We will insert to a container of size N.
+	static int[] Ns = {1000,2000,3000,4000,5000,6000,7000,8000,9000,1000};
 
 
-	// Utility class: UlyssesScanner
 
-	protected static class UlyssesScanner {
-		String filename;
-		Scanner s;
-		public UlyssesScanner() throws FileNotFoundException { 
-			this("ulysses.txt");
+	// Utility class: word generator
+	
+	protected static class WordGenerator { 
+		Random r;
+		public WordGenerator() { 
+			r = new Random();
 		}
-		public UlyssesScanner(String f) throws FileNotFoundException {
-			filename = f;
-			s = new Scanner(new BufferedReader(new FileReader(filename)));
-		}
-		public boolean hasNext() {
-			return s.hasNext();
-		}
-		public String next() {
-			return s.next();
-		}
-	}
-
-	protected static class OxenScanner extends UlyssesScanner { 
-		public OxenScanner() throws FileNotFoundException { 
-			super("../14oxenofthesun.txt");
+		public String next() { 
+			int n = r.nextInt(15);
+			char[] result = new char[n];
+			for(int j=0; j<n; j++) { 
+				result[j] = (char)('a' + r.nextInt(26));
+			}
+			return new String(result);
 		}
 	}
 
@@ -67,8 +59,8 @@ public class TimingMap {
 		Random r = new Random();
 
 
-		System.out.print("N, Walltime Add Tot (ms), Walltime Add Per 1k (ms), ");
-		System.out.println("Walltime Rm Tot (ms), Walltime Rm Per 1k (ms)");
+		System.out.print("N, Walltime Insert Tot (ms), Walltime Insert Per 1k (ms), ");
+		System.out.print("Walltime Rm Tot (ms), Walltime Rm Per 1k (ms)\n");
 
 
 		// Gathering stats on each value of N
@@ -78,38 +70,33 @@ public class TimingMap {
 			int N = Ns[i];
 
 			// set timer
-			Tim add_tim = new Tim();
-			Tim rm_tim  = new Tim();
+			Tim insert_tim = new Tim();
+			Tim rm_tim     = new Tim();
 
 			for(int k=0; k<Ntrials; k++) { 
 
+				m.clear();
+
 				// Initialize the container to be of size N
-				UlyssesScanner ulysses = new UlyssesScanner();
+				WordGenerator wg = new WordGenerator();
 				// (this actually isn't correct... duplicates mean we are smaller than N. 
 				//  could alternatively do this wile size()<N.)
 				for(int j=0; j<N; j++) { 
-					if(ulysses.hasNext()) { 
-						String token = ulysses.next();
-						m.put(token, token.toUpperCase());
-					}
+					String token = wg.next();
+					m.put(token, token.toUpperCase());
 				}
 
 
-				// Now time adding
-				add_tim.tic();
+
+				// Now time inserting
+				insert_tim.tic();
 
 				for(int j=0; j<Nops; j++) { 
-					if(ulysses.hasNext()) { 
-						String token = ulysses.next();
-						m.put(token, token.toUpperCase());
-					}
+					String token = wg.next();
+					m.put(token, token.toUpperCase());
 				}
 
-				add_tim.toc();
-
-
-
-				OxenScanner ox = new OxenScanner();
+				insert_tim.toc();
 
 
 				// Now time removing
@@ -117,28 +104,26 @@ public class TimingMap {
 
 
 				for(int j=0; j<Nops; j++) { 
-					if(ox.hasNext()) { 
-						String token = ox.next();
-						m.remove(token);
-					}
+					String token = wg.next();
+					m.put(token, token.toUpperCase());
 				}
 
 				rm_tim.toc();
 
 			}
 
-			// Now printing out N, total walltime, time per 1k add ops
-			//double add_per_op = add_tim.elapsedms()/(1.0*Ntrials*Nops);
-			double add_per_1kop = (add_tim.elapsedms()/(1.0*Ntrials*Nops))*1000;
+			// Now printing out N, total walltime, time per 1k insert ops
+			//double insert_per_op = insert_tim.elapsedms()/(1.0*Ntrials*Nops);
+			double insert_per_1kop = (insert_tim.elapsedms()/(1.0*Ntrials*Nops))*1000;
 			double rm_per_1kop  = (rm_tim.elapsedms()/(1.0*Ntrials*Nops))*1000;
 
 			// done with timing trials, now print out
-			// N, add total, ad per 1k operation
+			// N, insert total, ad per 1k operation
 
 
 
-			// Add:
-			System.out.printf("%d, %.4f, %.4f, ", N, add_tim.elapsedms(), add_per_1kop);
+			// Insert:
+			System.out.printf("%d, %.4f, %.4f, ", N, insert_tim.elapsedms(), insert_per_1kop);
 
 
 			// Remove:
